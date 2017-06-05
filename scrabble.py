77012fe64ab3258702dfd7a5dbad1ac140ee09dc
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-06-05 22:59:37 +0530

    Update scrabble.py
    Improved version of scrabble.py

--- scrabble.py
+++ scrabble.py
@@ -1,7 +1,6 @@
-import itertools
+import argparse
 
 valid_words = []
-words = []
 l = []
 dic = {}
 score = 0
@@ -11,29 +10,40 @@ scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
          "x": 8, "z": 10}                       #dict of the scores assigned to each English alphabet
 
+##### Argument parser 
+"""parser = argparse.ArgumentParser()   
+parser.add_argument("echo", help="Echo the wrack of letters", type=str)
+args = parser.parse_args()
+print args.echo
+rack = args.echo"""
 
-rack = raw_input('Enter rack of words: ')       #taking rack of word from the user
-fp = open('sowpods.txt', 'r')                   #reading SOWPODS file
+rack = raw_input('Enter rack of words: ').lower()       #taking rack of word from the user
+fp = open('sowpods.txt', 'r')                           #reading SOWPODS file
 
 for line in fp.readlines():
-    word = line.strip()
-    words.append(word)                          #making list of all the words in the file
+    flag = 0
+    dup_rack = rack
+    word = line.strip()                         #Handling every valid word in the file
+    for letter in word.lower():
+        if letter not in dup_rack:
+            flag = 1
+            break
+        elif letter in dup_rack:
+            dup_rack = dup_rack.replace(letter, '')     #removing the letter that is already checked from rack var
+    if flag == 0:
+        valid_words.append(word.lower())                #list of all the valid words
 
-for i in range(1, len(rack)+1):
-    l = list(itertools.permutations(rack, i))   #finding permutations of the rack of all possible lengths
-    for key in l:
-        string = ''.join(map(str, key))
-        #print string
-        if string.upper() in words:             #checking whether word is present in the file
-            valid_words.append(string)          #if YES, it's a valid word and so appended to the list 
 
 fp.close()
 for word in set(valid_words):
     for letter in word.lower():
-        score += scores[letter]                 #calculating scores per word
-    dic.update({word : score})                  #adding valid words with their scores to the dict 
+        score += scores[letter]                 #calculating the scores per word
+    dic.update({word : score})                  #adding valid words with their scores to the dictionary 
     score = 0
-    
-print 'All the valid words are....'
-for key in sorted(dic):
-    print dic[key], key                         #printing score and valid word pair from the dict
+
+if dic == {}:
+    print 'No valid word found!'
+else:
+    print 'All the valid words are....'
+    for key in sorted(dic):
+        print dic[key], key                         #printing score and valid word pair from the dict
